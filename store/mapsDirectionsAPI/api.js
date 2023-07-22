4788149907daaadb43e2dd7087d5d80652263c65
Crowdbotics <engineering@crowdbotics.com>
2023-07-22 11:51:59 +0000

    Update store.

--- store/mapsDirectionsAPI/api.js
+++ store/mapsDirectionsAPI/api.js
@@ -1,12 +1,8 @@
-import axios from "axios";
+import axios from "axios"
 const mapsDirectionsAPI = axios.create({
   baseURL: "https://maps.googleapis.com",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function mapsdirectionsapi_get_maps_api_directions_json_list(payload) {
   return mapsDirectionsAPI.get(`/maps/api/directions/json`, {
     params: {
@@ -14,9 +10,8 @@ function mapsdirectionsapi_get_maps_api_directions_json_list(payload) {
       origin: payload.origin,
       key: payload.key
     }
-  });
+  })
 }
-
 export const apiService = {
   mapsdirectionsapi_get_maps_api_directions_json_list
-};
+}
